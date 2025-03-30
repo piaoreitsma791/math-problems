6ab74eea0ead9615ccb23608bcf455dc35c58780
piaoreitsma791 <piaoreitsma791@gmail.com>
2025-03-30 15:59:37 +0000

    Add 20.ts

---
+++ src/20.ts
@@ -0,0 +1,10 @@
+from math import sin, cos, pi
+
+def polar_to_cartesian(r, theta):
+    x = r * cos(theta)
+    y = r * sin(theta)
+    return (x, y)
+
+r = 3
+theta = pi / 2
+print(polar_to_cartesian(r, theta))
