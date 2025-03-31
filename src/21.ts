1d133b9c3e1243408df957f3424931bd6980fe6d
piaoreitsma791 <piaoreitsma791@gmail.com>
2025-03-31 16:20:52 +0000

    Add 21.ts

---
+++ src/21.ts
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.datasets import load_iris
+
+data = load_iris()
+x_train, y_train = data.data[:100], data.target[:100]
+y_test, _ = data.target[100:], data.target[-1]
+
+def generate_random_tsp_code():
+    # Generate a random transportation problem
+    tsp = np.random.randint(0, 3, size=(len(x_train), len(y_train)))
+    for i in range(len(x_train)):
+        x_i, y_i = x_train[i], y_train[i]
+        tsp[i][i] = 1
+
+    return tsp
+
+# Generate the code
+random_tsp_code = generate_random_tsp_code()
+print(random_tsp_code)
